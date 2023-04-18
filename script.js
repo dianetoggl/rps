9c304e603514293f90c4363083725a40ae3ea6ca
Diane <diane.thompson@toggl.com>
2023-04-18 13:07:52 +1200

    Aded player prompt

--- script.js
+++ script.js
@@ -2,15 +2,23 @@ function getComputerChoice () {
     let computerChoice = Math.floor(Math.random() *3)
     switch (computerChoice) {
         case 0:
-        return "Rock"
+        return "rock"
         break;
         case 1: 
-        return "Paper"
+        return "paper"
         break;
         case 2: 
-        return "Scissors"
+        return "scissors"
         break;
     }
 }
 
 let cpuChoice = getComputerChoice()
+let playerChoice = prompt("Choose your move!")
+
+function playRound(playerChoice, cpuChoice) {
+    playerChoice.toLowerCase
+    if (playerChoice == cpuChoice) {
+        console.log("it's a draw!")
+    }
+}
